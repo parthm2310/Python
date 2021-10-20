d1d3b0002cd5f7e08fa883ef37389e580eca2725
parthm2310 <86843036+parthm2310@users.noreply.github.com>
2021-10-20 14:47:28 +0530

    Update radius.py

--- radius.py
+++ radius.py
@@ -2,4 +2,4 @@ from math import pi
 
 r = float(input ("Enter radius of circle : "))
 
-print ("Area of the circle with radius " +str(r) +" is : " + str(pi * r**2))
+print ("Area of the circle with radius %d is : %f"%(r,pi * r**2))
