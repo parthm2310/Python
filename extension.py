e7fb0046b121a7f045a573c5153184d5e6c29ba7
parthm2310 <86843036+parthm2310@users.noreply.github.com>
2021-10-20 16:39:02 +0530

    Update extension.py

--- extension.py
+++ extension.py
@@ -1,3 +1,15 @@
 filename = input("Input the Filename: ")
-f_extns = filename.split(".")
-print ("The extension of the file is : " + repr(f_extns[-1]))
+
+filename,separator,extension=filename.partition('.')
+
+if(extension=='py'):
+    print("The extension of the file is : 'python'")
+
+elif(extension=='c'):
+    print("The extension of the file is : 'C'")
+    
+elif(extension=='cpp'):
+    print("The extension of the file is : 'C++'")
+
+elif(extension=='java'):
+    print("The extension of the file is : 'java'")
